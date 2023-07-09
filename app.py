f02185d93ebde8d48941c942ea31962f14b80a9d
Tamás Vince Kornél <me@vinn.fun>
2023-07-09 19:48:12 +0000

    rate limiting, fancier error pages

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import os
 from uuid import uuid4
 
 import magic
+from functools import wraps
 from colorama import Fore, Style, init
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory)
 from flask_cors import CORS
 from PIL import Image
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 CORS(app)
@@ -24,6 +27,13 @@ logger.setLevel(logging.DEBUG)
 werkzeug_logger = logging.getLogger('werkzeug')
 werkzeug_logger.setLevel(logging.ERROR)
 
+# Initialize Flask-Limiter
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    storage_uri="memory://",
+)
+
 def check_request(request):
     if 'file' not in request.files:
         return [False, 400, 'No file uploaded\n'] 
@@ -33,27 +43,43 @@ def check_request(request):
         return [False, 400, 'Empty file name\n']
 
     if not ('.' in file.filename and file.filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg', 'gif', 'bmp'}):
-        return [False, 403, 'File extension not allowed\n']
+        return [False, 415, 'File extension not allowed\n']
 
+    if not magic.from_buffer(file.read(), mime=True).startswith('image/'):
+        return [False, 400,  'Invalid image file\n']
+    file.seek(0)
     return [True, file]
+
+@app.errorhandler(413)
+def request_entity_too_large():
+    if request.args.get('s'):
+        return render_template('index.html', error=[413, "File size too large"]), 413
+    return "File size too large\n", 413
+
+@app.errorhandler(429)
+def too_many_requests(*args, **kwargs):
+    if request.args.get('s'):
+        return render_template('index.html', error=[429, "Too many requests"]), 429
+    return "Too many requests\n", 429
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('404.html', error="page not found"), 404
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
+@limiter.limit("40/minute", override_defaults=False)
 def upload():
     check = check_request(request)
     if not check[0]:
-        logger.error(f"{Fore.RED}Upload failed: {check[2]}{Style.RESET_ALL}")  # Log error message
+        logger.error(f"{Fore.RED}Upload failed: {check[2]}{Style.RESET_ALL}")
+        if request.args.get('s'):
+            return render_template('index.html', error=check[1:]), check[1]
         return check[2], check[1]
-
-
     file = check[1]
-
-    if not magic.from_buffer(file.read(), mime=True).startswith('image/'):
-        return 'Invalid image file\n', 400
-    file.seek(0)
 
     image = Image.open(file)
     image = image.convert('RGB')
@@ -81,7 +107,7 @@ def image(image_id):
     image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_id + '.jpg')
     if not os.path.exists(image_path):
         logger.error(f"{Fore.RED}Image not found: {image_id}{Style.RESET_ALL}")  # Log image not found
-        return 'Image not found', 404
+        return render_template('404.html', error="image not found"), 404
     else:
         return send_from_directory(app.config['UPLOAD_FOLDER'], image_id + '.jpg')
 
